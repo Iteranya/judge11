55163741be68e250bfd264fd36c4e808696ad848
Ars Paradox <pandudparadox@gmail.com>
2026-05-29 09:47:04 +0700

    fix legacy code

--- legacy_main.py
+++ legacy_main.py
@@ -172,4 +172,4 @@ async def test_judge(body: TestCode):
             }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5893, reload=False)
+    uvicorn.run("legacy_main:app", host="127.0.0.1", port=5893, reload=False)
